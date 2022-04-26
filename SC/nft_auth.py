929eb75291a6c554a0dd405f3439247d1f6b9bfe
LFL <lfl@numericable.fr>
2022-04-26 20:47:51 +0200

    suppression commentaires SC

--- SC/nft_auth.py
+++ SC/nft_auth.py
@@ -71,25 +71,3 @@ def test():
                     ])
         ]).run(sender = CFGZ1)
 
-
-""""
-    consumer = FA2.View_consumer(auth_nft)
-    scenario += consumer
-
-    def arguments_for_balance_of(receiver, reqs):
-        return (sp.record(
-                        callback = sp.contract(
-                            FA2.Balance_of.response_type(),
-                            sp.contract_address(receiver),
-                            entry_point = "receive_balances").open_some(),
-                        requests = reqs
-                        )
-                )
-
-
-    scenario += auth_nft.balance_of(arguments_for_balance_of(
-        consumer, 
-        [sp.record(owner = CFGZ1.address, token_id = 1)]
-        )
-    )
-""""
