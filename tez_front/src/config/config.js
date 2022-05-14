e3d1079c6df44c3716e346ca433b5af417546200
LFL <lfl@numericable.fr>
2022-05-14 13:27:42 +0200

    first contract deployed

--- tez_front/src/config/config.js
+++ tez_front/src/config/config.js
@@ -1,5 +1,5 @@
 
 export const NAME = 'TezAuth'; // the name of the app
-export const CONTRACT_ADDRESS = ''; // une fois déployé
-export const RPC_URL = 'jakartanet.smartpy.io'; // network rpc url
-export const NETWORK = jakartanet;
+export const CONTRACT_ADDRESS = 'KT1SESzAVAhesyaaX18S3CNk51Vq182pcLuQ'; // une fois déployé
+export const RPC_URL = 'ithacanet.smartpy.io'; // network rpc url
+export const NETWORK = ithacanet;
