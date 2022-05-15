09af832ed98f243e85719501e2354e825505d371
LFL <lfl@numericable.fr>
2022-05-15 13:30:19 +0200

    correction addresse SC

--- tez_front/src/config/config.js
+++ tez_front/src/config/config.js
@@ -1,6 +1,6 @@
 
 export const NAME = 'TezAuth'; // the name of the app
-export const CONTRACT_ADDRESS = 'KT1VonNS58CrkusMMyGkjyDnisRXDa18fc6X'; // une fois déployé
+export const CONTRACT_ADDRESS = 'KT1Ki7b9KoAtjxiPmBgTzNCpxnw1PgCgnLM3'; // une fois déployé
 export const RPC_URL = 'ithacanet.smartpy.io'; // network rpc url
 export const NETWORK = ithacanet;
 
