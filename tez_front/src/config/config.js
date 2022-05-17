b277cf282e662b21ea3457fed7adbb5d229ec715
LFL <lfl@numericable.fr>
2022-05-17 19:31:42 +0200

    sc

--- tez_front/src/config/config.js
+++ tez_front/src/config/config.js
@@ -1,6 +1,6 @@
 
 export const NAME = 'TezAuth'; // the name of the app
-export const CONTRACT_ADDRESS = 'KT1J2Hu8YYSC4YkcNG6HGXQZ26aQ5RFQy56H'; // une fois déployé
+export const CONTRACT_ADDRESS = 'KT1XHTXV8WMgQ6QzGxfQ61oN9TPbYftEUGVX'; // une fois déployé
 export const RPC_URL = 'ithacanet.smartpy.io'; // network rpc url
 export const NETWORK = ithacanet;
 
