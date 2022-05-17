818284252e0a3d7611e15357d723c0b463771807
LFL <lfl@numericable.fr>
2022-05-17 22:48:37 +0200

    dossier test ajouté

---
+++ Test/web3/config/config.js
@@ -0,0 +1,7 @@
+
+export const NAME = 'TezAuth'; // the name of the app
+export const CONTRACT_ADDRESS = 'KT1XHTXV8WMgQ6QzGxfQ61oN9TPbYftEUGVX'; // une fois déployé
+export const RPC_URL = 'ithacanet.smartpy.io'; // network rpc url
+export const NETWORK = ithacanet;
+
+
